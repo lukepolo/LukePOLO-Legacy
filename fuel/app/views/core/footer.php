922bdd991996b618a1a2bd9b2e115dc48c4086de
Luke Policinski <lpolicinski@gmail.com>
2013-06-06 16:02:34 +0000

    tottally fixed footer now alignments and other media css issues were handled

--- fuel/app/views/core/footer.php
+++ fuel/app/views/core/footer.php
@@ -2,7 +2,8 @@
 	<div class="container">
 		<div class="row" id="footer_contents">
 			<div class="span3">
-				<a href="https://twitter.com/share" class="twitter-share-button" data-via="lpolicin">Tweet</a>
+				<a href="https://twitter.com/share" class="twitter-share-button" data-url="https://LukePOLO.com" data-text="Check out this site ! ">">Tweet</a>
+				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 				<br>
 				<small>FuelPHP Version: <?php echo e(Fuel::VERSION); ?></small>
 			</div>
@@ -10,7 +11,7 @@
 				<a href="https://www.digitalocean.com/?refcode=27ffab8712be"><?php echo Asset::img('digital_ocean/digitalocean-badge-white.png'); ?></a>
 				<br>
 			</div>
-			<div class="span3 offset2">
+			<div class="span3 offset2" id="footer_right_contents">
 				Page rendered in {exec_time}s <br> Using {mem_usage}mb of memory
 			</div>
 			<br>
