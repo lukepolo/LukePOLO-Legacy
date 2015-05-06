ae4bbacecab410f5db62fa696bf7b0f54c2b4184
LukePOLO <Luke@LukePOLO.com>
2015-05-06 14:18:11 -0400

    fixing bad img for dg

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,7 +4,7 @@
         </div>
         <div class="col-md-2">
             <a target="_blank" href="https://www.digitalocean.com/">
-                <img src="http://lukepolo.com/assets/img/digital_ocean/digitalocean-badge-white.png?1424209025">
+                <img src="/img/digital_ocean/digitalocean-badge-white.png?1424209025">
             </a>
         </div>
         <div class="col-md-2">
