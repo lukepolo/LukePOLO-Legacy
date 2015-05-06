fe69194f0035db0fc2095e1a404d25028e0d66f1
LukePOLO <Luke@LukePOLO.com>
2015-05-06 19:46:39 -0400

    quick fix for mobile comments moderation, also quick fix for comments into admin section

--- resources/views/admin/comment.blade.php
+++ resources/views/admin/comment.blade.php
@@ -1,10 +1,10 @@
 @if(\Auth::user()->id != $comment->user_id)
     <div data-id="{{ $comment->id }}" class="comment-row">
         <div class="row">
-            <div class="col-sm-2">
+            <div class="col-xs-2">
                 <img class="pull-right img-responsive" src="{{ empty($comment->user->profile_img) === false ? $comment->user->profile_img : asset('/img/user.svg') }}">
             </div>
-            <div class="col-sm-10">
+            <div class="col-xs-10">
                 <div class="row">
                     <span class="user-name">
                         {{ $comment->user->first_name }}
