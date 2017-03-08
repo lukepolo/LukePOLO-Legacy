2ae95a321f6661e32663814dfc519ab03135546b
Luke Policinski <luke@lukepolo.com>
2017-03-08 16:21:03 +0000

    fixing resume page

--- resources/views/resume.blade.php
+++ resources/views/resume.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.public')
 @section('content')
     <a target="_blank" href="https://dl.dropboxusercontent.com/u/20485770/Luke%20Policinski.pdf">Download PDF</a>
-    <iframe class="no-border" src="https://docs.google.com/viewer?url=https://dl.dropboxusercontent.com/u/20485770/Luke%20Policinski.pdf&embedded=true" width="100%" height="1080"></iframe>
+    <iframe class="no-border" src="https://docs.google.com/viewer?url=https://www.dropbox.com/s/4q6urgnixx8tdb0/Luke%20Policinski.pdf?dl=1&embedded=true" width="100%" height="1080"></iframe>
 @endsection
